c0731eff06fc6cb027ce7c9c09ac937a83eaa35e
01000001-01001110 <alan.newingham@gmail.com>
2020-03-10 11:45:12 -0400

    Inital Commit

---
+++ DellServiceTag/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace DellServiceTag
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Display = new System.Windows.Forms.TextBox();
+            this.DisplayLabel = new System.Windows.Forms.Label();
+            this.DriversLink = new System.Windows.Forms.LinkLabel();
+            this.warrantyLink = new System.Windows.Forms.LinkLabel();
+            this.ExpressCodeLabel = new System.Windows.Forms.Label();
+            this.ExpressTxtBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Display
+            // 
+            this.Display.Location = new System.Drawing.Point(88, 12);
+            this.Display.Name = "Display";
+            this.Display.ReadOnly = true;
+            this.Display.Size = new System.Drawing.Size(121, 20);
+            this.Display.TabIndex = 1;
+            this.Display.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // DisplayLabel
+            // 
+            this.DisplayLabel.AutoSize = true;
+            this.DisplayLabel.Location = new System.Drawing.Point(5, 19);
+            this.DisplayLabel.Name = "DisplayLabel";
+            this.DisplayLabel.Size = new System.Drawing.Size(68, 13);
+            this.DisplayLabel.TabIndex = 2;
+            this.DisplayLabel.Text = "Service Tag:";
+            // 
+            // DriversLink
+            // 
+            this.DriversLink.AutoSize = true;
+            this.DriversLink.Location = new System.Drawing.Point(85, 67);
+            this.DriversLink.Name = "DriversLink";
+            this.DriversLink.Size = new System.Drawing.Size(40, 13);
+            this.DriversLink.TabIndex = 3;
+            this.DriversLink.TabStop = true;
+            this.DriversLink.Text = "Drivers";
+            this.DriversLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.DriversLink_LinkClicked);
+            // 
+            // warrantyLink
+            // 
+            this.warrantyLink.AutoSize = true;
+            this.warrantyLink.Location = new System.Drawing.Point(159, 67);
+            this.warrantyLink.Name = "warrantyLink";
+            this.warrantyLink.Size = new System.Drawing.Size(50, 13);
+            this.warrantyLink.TabIndex = 4;
+            this.warrantyLink.TabStop = true;
+            this.warrantyLink.Text = "Warranty";
+            this.warrantyLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.warrantyLink_LinkClicked);
+            // 
+            // ExpressCodeLabel
+            // 
+            this.ExpressCodeLabel.AutoSize = true;
+            this.ExpressCodeLabel.Location = new System.Drawing.Point(5, 47);
+            this.ExpressCodeLabel.Name = "ExpressCodeLabel";
+            this.ExpressCodeLabel.Size = new System.Drawing.Size(75, 13);
+            this.ExpressCodeLabel.TabIndex = 5;
+            this.ExpressCodeLabel.Text = "Express Code:";
+            // 
+            // ExpressTxtBox
+            // 
+            this.ExpressTxtBox.Location = new System.Drawing.Point(88, 44);
+            this.ExpressTxtBox.Name = "ExpressTxtBox";
+            this.ExpressTxtBox.ReadOnly = true;
+            this.ExpressTxtBox.Size = new System.Drawing.Size(121, 20);
+            this.ExpressTxtBox.TabIndex = 6;
+            this.ExpressTxtBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(222, 86);
+            this.Controls.Add(this.ExpressTxtBox);
+            this.Controls.Add(this.ExpressCodeLabel);
+            this.Controls.Add(this.warrantyLink);
+            this.Controls.Add(this.DriversLink);
+            this.Controls.Add(this.DisplayLabel);
+            this.Controls.Add(this.Display);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dell Service Lookup";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Display;
+        private System.Windows.Forms.Label DisplayLabel;
+        private System.Windows.Forms.LinkLabel DriversLink;
+        private System.Windows.Forms.LinkLabel warrantyLink;
+        private System.Windows.Forms.Label ExpressCodeLabel;
+        private System.Windows.Forms.TextBox ExpressTxtBox;
+    }
+}
+
---
+++ DellServiceTag/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Management;
+
+namespace DellServiceTag
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        String dellServiceTag;
+        String expressCode;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Load Service Tag
+            ManagementClass wmi = new ManagementClass("Win32_Bios");
+            foreach (ManagementObject bios in wmi.GetInstances())
+            {
+                dellServiceTag = bios.Properties["Serialnumber"].Value.ToString().Trim();
+            }
+            Display.Text = dellServiceTag;
+            expressCode = base36decode(dellServiceTag).ToString("###-###-###-##");
+            ExpressTxtBox.Text = expressCode;
+        }
+
+        private void DriversLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.dell.com/support/home/us/en/04/product-support/servicetag/" + dellServiceTag + "/drivers");
+        }
+
+        private void warrantyLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.dell.com/support/home/us/en/04/product-support/servicetag/" + dellServiceTag +"/overview");
+        }
+
+        private long base36decode(string inputString)
+        {
+            string clist = "0123456789abcdefghijklmnopqrstuvwxyz";
+            inputString = Reverse(inputString.ToLower());
+            long result = 0;
+            int pos = 0;
+            foreach (char c in inputString)
+            {
+                result += clist.IndexOf(c) * (long)Math.Pow(36, pos);
+                pos++;
+            }
+            return result;
+        }
+
+        private string Reverse(string input)
+        {
+            char[] arr = input.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+        
+    }
+}
+      
