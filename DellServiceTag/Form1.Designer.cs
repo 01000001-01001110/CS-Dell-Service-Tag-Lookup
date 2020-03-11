6379c7a7431983ec477b7a7de09c7d286549c101
01000001-01001110 <alan.newingham@gmail.com>
2020-03-11 09:01:15 -0400

    .

--- DellServiceTag/Form1.Designer.cs
+++ DellServiceTag/Form1.Designer.cs
@@ -123,7 +123,7 @@
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Dell Device Lookup Tool";
+            this.Text = "Dell Service Tag Lookup Tool ";
             this.TopMost = true;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
